2a2031dd1cecc432ee7509ecc523cb06a6044b70
Victor Castro <vmcastrom@gmail.com>
2018-05-31 14:04:19 -0500

    Controllers, models and repository

---
+++ ContactsAPI/Repository/IContactsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsAPI.Repository
+{
+    interface IContactsRepository
+    {
+        void Add(Contacts item);
+        IEnumerable<Contacts> GetAll();
+        Contacts Find(string key);
+        void Remove(string Id);
+        void Update(Contacts item);
+    }
+}
